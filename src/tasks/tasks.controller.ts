fc64fac55182ee85fc72f0d87706bed273966a8b
iarmankhan <armankhan9244@gmail.com>
2021-02-09 00:00:03 +0530

    :sparkles: create task not empty validation

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,6 +7,8 @@ import {
   Patch,
   Post,
   Query,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
@@ -31,6 +33,7 @@ export class TasksController {
   }
 
   @Post()
+  @UsePipes(ValidationPipe)
   createTask(@Body() createTaskDto: CreateTaskDto) {
     return this.tasksService.createTask(createTaskDto);
   }

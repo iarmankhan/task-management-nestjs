6b2ed4d030ad138a35039728a198bf2786e39abe
iarmankhan <armankhan9244@gmail.com>
2021-02-15 22:42:03 +0530

    :sparkles: Typeorm setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeormConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TypeOrmModule.forRoot(typeormConfig), TasksModule],
 })
 export class AppModule {}
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeormConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'task_management',
+  entities: [__dirname + '/../**/*.entity.ts'],
+  synchronize: true,
+};

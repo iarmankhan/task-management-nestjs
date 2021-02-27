8ee7a49641425b5a6e6894b6ed92de409b7d5390
iarmankhan <armankhan9244@gmail.com>
2021-02-27 21:28:12 +0530

    :construction: setting up auth module, user entity & repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeormConfig } from './config/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeormConfig), TasksModule],
+  imports: [TypeOrmModule.forRoot(typeormConfig), TasksModule, AuthModule],
 })
 export class AppModule {}

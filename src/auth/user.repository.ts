82d84573e516ab6c901fbc9d868c88b98e3dee24
iarmankhan <armankhan9244@gmail.com>
2021-02-28 18:45:03 +0530

    :sparkles: unique constraint on username column

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,11 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { ErrorCodes } from '../constants/errorCodes';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -10,6 +15,16 @@ export class UserRepository extends Repository<User> {
     const user = new User();
     user.username = username;
     user.password = password;
-    await user.save();
+
+    try {
+      await user.save();
+    } catch (error) {
+      // duplicate username
+      if (error.code === ErrorCodes.UNIQUE_VIOLATION) {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
---
+++ src/constants/errorCodes.ts
@@ -0,0 +1,3 @@
+export const ErrorCodes = {
+  UNIQUE_VIOLATION: '23505',
+};

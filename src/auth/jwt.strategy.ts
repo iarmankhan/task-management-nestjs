7ffeaf6f2604f6c615a91e2aae5103e74ba23cd7
iarmankhan <armankhan9244@gmail.com>
2021-04-11 23:16:34 +0530

    :sparkles: code base config

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -6,6 +6,10 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { User } from './user.entity';
 
+import * as config from 'config';
+
+const jwtConfig = config.get('jwt');
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
@@ -14,7 +18,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'topSecret51',
+      secretOrKey: process.env.JWT_SECRET || jwtConfig.secret,
     });
   }
 
